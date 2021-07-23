3af921a75ea4de4428d7b8de0c58973e9c4c0f3d
aravindhippili <aravindhippili@gmail.com>
2021-07-23 20:19:04 +0530

    fix: ui

--- lib/screens/home_video.dart
+++ lib/screens/home_video.dart
@@ -8,6 +8,7 @@ import 'package:aws_auth/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class HomeVideo extends StatefulWidget {
   const HomeVideo({Key? key}) : super(key: key);
@@ -25,103 +26,131 @@ class _HomeVideoState extends State<HomeVideo> {
         ? Loading()
         : Scaffold(
             appBar: AppBar(
-              backgroundColor: Colors.white,
               elevation: 0,
+              backgroundColor: Colors.transparent,
               actions: [
                 TextButton(
-                    onPressed: () async {
-                      try {
-                        setState(() {
-                          _isLoading = true;
-                        });
-                        final authAWSRepo =
-                            context.read(authAWSRepositoryProvider);
-                        await authAWSRepo.logOut();
-                        context.refresh(authUserProvider);
-                        setState(() {
-                          _isLoading = false;
-                        });
-                      } catch (e) {}
-                    },
-                    child: Text(
-                      "Logout",
-                    ))
+                  onPressed: () async {
+                    try {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      final authAWSRepo =
+                          context.read(authAWSRepositoryProvider);
+                      await authAWSRepo.logOut();
+                      context.refresh(authUserProvider);
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    } catch (e) {}
+                  },
+                  child: Text(
+                    "Logout",
+                  ),
+                )
               ],
             ),
             body: SingleChildScrollView(
               child: Container(
-                color: Colors.white,
                 padding: EdgeInsets.all(20),
                 child: Column(
                   children: [
                     Container(
-                      child: Text(
+                      child: GradientText(
                         "PlayZone",
-                        style: TextStyle(fontSize: kDefFontSize),
+                        colors: [
+                          Color(0xFF303030),
+                          Colors.white,
+                        ],
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
+                    SizedBox(height:30),
                     if (image != null)
-                      Image.file(File(image!.path))
-                    else
-                      Text(""),
+                      Container(
+                        padding: EdgeInsets.all(3),
+                        color: Colors.amber,
+                        child: Image.file(
+                          File(image!.path),
+                        ),
+                      ),
+                    SizedBox(height:10),
                     Container(
-                        child: TextButton(
-                            onPressed: () async {
-                              final ImagePicker _picker = ImagePicker();
-                              final XFile? img = await _picker.pickImage(
-                                  source: ImageSource.gallery,
-                                  imageQuality: 100,
-                                  maxHeight:
-                                      MediaQuery.of(context).size.height / 2);
-                              setState(() {
-                                image = img;
-                              });
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              width: double.infinity,
-                              alignment: Alignment.center,
-                              child: Text(
-                                "Select Image",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: kDefFontSize),
-                              ),
-                            ),
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.amber),
-                            ))),
+                      child: TextButton(
+                        onPressed: () async {
+                          final ImagePicker _picker = ImagePicker();
+                          final XFile? img = await _picker.pickImage(
+                              source: ImageSource.gallery,
+                              maxHeight:
+                                  MediaQuery.of(context).size.height / 2);
+                          setState(() {
+                            image = img;
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Select Image",
+                            style: TextStyle(
+                                color: Colors.black, fontSize: kDefFontSize),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.amber),
+                        ),
+                      ),
+                    ),
                     SizedBox(
                       height: 30,
                     ),
                     Container(
-                        child: TextButton(
-                            onPressed: () async {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ViewImages()));
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              width: double.infinity,
-                              alignment: Alignment.center,
-                              child: Text(
-                                "View Images",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: kDefFontSize),
-                              ),
+                      child: TextButton(
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ViewImages(),
                             ),
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.amber),
-                            ))),
-                    Container(
-                      child: Text(
-                        "Latest Feature in Streaming Your Favourite Channel",
-                        style: TextStyle(fontSize: kDefFontSize),
+                          );
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "View Images",
+                            style: TextStyle(
+                                color: Colors.black, fontSize: kDefFontSize),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.amber),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        child: Text(
+                          "Latest Feature in Streaming Your Favourite Channel",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     )
                   ],
