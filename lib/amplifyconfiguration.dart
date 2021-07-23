48820c08fb22a65c0ab9c3bc004dcd6b5f463813
aravindhippili <aravindhippili@gmail.com>
2021-07-23 12:48:38 +0530

    fix: apk build

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,35 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-2:74e37bef-a428-42cf-8be9-96f6ef6a5961",
+                            "Region": "us-east-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-2_tIDepRlMB",
+                        "AppClientId": "1h2g62s14p4ifed237kj7ef77t",
+                        "Region": "us-east-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
